fe39420d5c78bd66b75f5b62a58562c75fe9ba47
Muhammad Fauzan <50759463+fauzan121002@users.noreply.github.com>
2020-02-02 16:32:41 +0700

    LTS alpha-1

--- supports/index.js
+++ supports/index.js
@@ -1,7 +1,7 @@
 const mail = require('./mail/')
-const arr = require('./arr/')
-const str = require('./str/')
-const datetime = require('./datetime/')
+//const arr = require('./arr/')
+//const str = require('./str/')
+//const datetime = require('./datetime/')
 const redis = require('./redis/')
 
 module.exports = {mail,arr,str,datetime,redis}
